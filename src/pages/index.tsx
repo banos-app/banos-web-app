ff277b3b05fd6b36251a61109703a8bce4884bb7
jpmayorga <jpmayorga@outlook.com>
2021-12-22 00:36:17 -0500

    adding index key

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,8 +19,8 @@ export default function Home() {
           Popular places in Banos de Agua Santa
         </h2>
         <div className="text-center">
-          {new Array(20).fill(null).map((item) => (
-            <Link href="/">
+          {new Array(20).fill(null).map((item, index) => (
+            <Link href="/" key={index}>
               <a className="inline-block px-8 py-2 border border-2 border-gray-200 rounded-xl text-gray-400 text-lg ml-4 mb-4 hover:border-gray-400 hover:text-gray-600 active:border-gray-600 active: text-gray-700 transition duration-30">
                 {faker.address.city()}
               </a>
